7f5de097b709516dca26dde8a893933c14d1e4f6
David Morales Hernández <eltantillo@gmail.com>
2020-10-29 18:52:32 -0400

    [ADD] hide price and cost and display pricelist.items

---
+++ product_price_hide/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Morwi Encoders Consulting SA de CV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'Product Price Hide',
+    'version': '13.0.1.0.0',
+    'category': 'Sales/Sales',
+    'author': 'Morwi Encoders',
+    'summary': 'Hide price from product form',
+    'license': 'LGPL-3',
+    'depends': [ 'sale_management' ],
+    'description': "Hide the sales price from the product form and display the pricelist entries under the sales tab",
+    'data': [ 'views/product_views.xml' ],
+}
