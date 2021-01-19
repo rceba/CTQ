1e9d0de0bc4a07d8ae890964f6542925f124c8ca
ougc27 <oscar.garza@morwi.mx>
2021-01-19 10:36:04 -0600

    [ADD] ctq_customs: Add user's company to partners without parent_id

---
+++ ctq_customs/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Morwi Encoders Consulting SA de CV
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    'name': 'CTQ Customizations',
+    'category': 'Hidden',
+    'version': '13.0.1.0.0',
+    'description': """
+       Module add custom flows for CTQ
+        """,
+    'summary': """
+        Module add custom flows for CTQ
+        """,
+    'depends': ['base'],
+    'data': [],
+}
