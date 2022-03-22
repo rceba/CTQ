1117b82a5367f575cac67ff1e835cb9c81dab6f3
David Morales Hernández <eltantillo@gmail.com>
2022-03-21 20:55:59 -0400

    [ADD] portal form fields

--- ctq_customs/controllers/__init__.py
+++ ctq_customs/controllers/__init__.py
@@ -2,6 +2,4 @@
 # Copyright 2021 Morwi Encoders Consulting SA de CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from . import controllers
-from . import models
-from . import report
+from . import portal
