bc470ab00c4122c54849659ab957bd97f91878e4
Alejandro <auralr@gmail.com>
2025-06-06 17:01:12 -0600

    [MIG] ctq_customs: Migration to 17.0

--- ctq_customs/__manifest__.py
+++ ctq_customs/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2021 Morwi Encoders Consulting SA de CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'CTQ Customizations',
     'category': 'Hidden',
-    'version': '15.0.1.0.0',
+    'version': "17.0.1.0.0",
     'description': """
        Module add custom flows for CTQ
         """,
--- ctq_customs/models/res_partner.py
+++ ctq_customs/models/res_partner.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Morwi Encoders Consulting SA de CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
