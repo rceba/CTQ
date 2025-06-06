b965d37747fc1d3b3e9ad1034a8c89c05007feb3
Alejandro <auralr@gmail.com>
2025-06-06 17:01:12 -0600

    [MIG] sale_keep_date: Migration to 17.0

--- sale_keep_date/__manifest__.py
+++ sale_keep_date/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Morwi Encoders Consulting SA de CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Sale Keep Date',
     'category': 'Sales/Sales',
-    'version': '13.0.1.0.0',
+    'version': "17.0.1.0.0",
     'description': """
        Module to avoid the override of the date_order field
         """,
