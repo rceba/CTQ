38cee0716335c6d27df93036405995136a800117
Alejandro <auralr@gmail.com>
2025-06-06 17:01:09 -0600

    [MIG] product_price_hide: Migration to 17.0

--- product_price_hide/__manifest__.py
+++ product_price_hide/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 Morwi Encoders Consulting SA de CV
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     'name': 'Product Price Hide',
-    'version': '13.0.1.0.0',
+    'version': "17.0.1.0.0",
     'category': 'Sales/Sales',
     'author': 'Morwi Encoders',
     'summary': 'Hide price from product form',
