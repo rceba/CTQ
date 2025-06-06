7b39820c70128ba28671924eaca7a3a8efbe9d5d
Alejandro <auralr@gmail.com>
2025-06-06 16:57:12 -0600

    [ADD] modulo para ayuda en futuras migraciones

---
+++ drs_base/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "DRASI | drs_base",
+    "summary": "Migrations tools",
+    "description": """
+        This module add scripts for migrations
+    """,
+    "author": "Drasi Consulting",
+    "website": "https://www.drasi.odoo.com",
+    "category": "Tools",
+    "version": "15.0.1.0.0",
+    "depends": ["base"],
+    "data": [],
+    "application": True,
+    "installable": True,
+}
