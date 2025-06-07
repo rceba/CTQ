024fba3d886921e21e762a57f12cff1afb231872
Alejandro <auralr@gmail.com>
2025-06-07 00:34:51 -0600

    Eliminando modulos

--- ctq_customs/models/utm.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2021 Morwi Encoders Consulting SA de CV
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
-from odoo import fields, models
-
-class UtmSource(models.Model):
-    _inherit = 'utm.source'
-
-    active = fields.Boolean(default=True)
--- sale_keep_date/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2020 Morwi Encoders Consulting SA de CV
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
-from . import models
