abb2fa7c928e8fcdc957f4dbe0ad66b1fcad4a8d
Alejandro <auralr@gmail.com>
2025-06-07 11:44:54 -0600

    Crear un contrato por defecto para ponerlo en las entradas de trabajo sin contrato

--- drs_base/migrations/17.0.1.0.1/post-uninstall_modules.py
+++ drs_base/migrations/17.0.1.0.1/post-uninstall_modules.py
@@ -5,10 +5,12 @@ from odoo.upgrade import util
 _logger = logging.getLogger("DRS_MIG")
 
 
-# def migrate(cr, version):
-#     util.parallel_execute(
-#         cr,
-#         [
-#             "DELETE FROM hr_work_entry WHERE contract_id IS NULL"
-#         ],
-#     )
+def migrate(cr, version):
+    def_contract = util.ref(cr, "hr_contract_migration")
+    if def_contract:
+        util.parallel_execute(
+            cr,
+            [
+                "UPDATE hr_work_entry SET contract_id=%s WHERE contract_id IS NULL" % def_contract
+            ],
+        )
