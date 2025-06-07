79e1cefc94aef45ca82cdfe674f10f123dbda1a6
Alejandro <auralr@gmail.com>
2025-06-07 00:49:49 -0600

    Script de migracion

--- drs_base/migrations/17.0.1.0.1/pre-uninstall_modules.py
+++ drs_base/migrations/17.0.1.0.1/pre-uninstall_modules.py
@@ -17,6 +17,16 @@ def migrate(cr, version):
     util.parallel_execute(
         cr,
         [
-            f"UPDATE ir_module_module set state='to remove' WHERE name in {tuple([module for module in modules_to_remove if util.module_installed(cr, module)])}",
+            "DELETE FROM ir_ui_view WHERE name='Odoo Studio: sale.order.form customization'",
+            "DELETE FROM ir_ui_view WHERE name='Odoo Studio: sale.order.search.inherit.quotation customization'",
+            "DELETE FROM ir_ui_view WHERE name='Odoo Studio: sale.order.search.inherit.sale customization'",
+            "DELETE FROM ir_ui_view WHERE name='Odoo Studio: sale.order.tree customization'",
+            "DELETE FROM ir_ui_view WHERE name='Odoo Studio: account.invoice.tree customization'",
+            "DELETE FROM ir_ui_view WHERE name='Odoo Studio: account.payment.tree customization'",
         ],
     )
+
+    for module in modules_to_remove:
+        if util.module_installed(cr, module):
+            util.remove_module(cr, module)
+            _logger.info(f"Eliminado modulo {module}.")
