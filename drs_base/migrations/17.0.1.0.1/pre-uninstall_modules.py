72a691a59fedea9e23d9fe83f32bd5958e1764e8
Alejandro <auralr@gmail.com>
2025-06-07 00:01:29 -0600

    [FIX] migration script

--- drs_base/migrations/17.0.1.0.1/pre-uninstall_modules.py
+++ drs_base/migrations/17.0.1.0.1/pre-uninstall_modules.py
@@ -6,7 +6,20 @@ _logger = logging.getLogger("DRS_MIG")
 
 
 def migrate(cr, version):
-    modules_to_remove = ["auditlog", "ctq_customs", "mail_tracking", "product_price_hide", "sale_keep_date"]
+    modules_to_remove = [
+        "auditlog",
+        "ctq_customs",
+        "mail_tracking",
+        "product_price_hide",
+        "sale_keep_date",
+    ]
+
+    util.parallel_execute(
+        cr,
+        [
+            f"UPDATE ir_module_module set state='to remove' WHERE name in {tuple([module for module in modules_to_remove if util.module_installed(cr, module)])}",
+        ],
+    )
 
     for module in modules_to_remove:
         if util.module_installed(cr, module):
