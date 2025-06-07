d94e55e0319983260a10930ecc05338494753a23
Alejandro <auralr@gmail.com>
2025-06-06 23:49:30 -0600

    migration tool

--- drs_base/migrations/17.0.1.0.1/pre-uninstall_modules.py
+++ drs_base/migrations/17.0.1.0.1/pre-uninstall_modules.py
@@ -5,5 +5,5 @@ def migrate(cr, version):
     modules_to_remove = ["auditlog", "ctq_customs", "mail_tracking", "product_price_hide", "sale_keep_date"]
 
     for module in modules_to_remove:
-        if util.is_module_installed(cr, module):
+        if util.module_installed(cr, module):
             util.uninstall_module(cr, module)
