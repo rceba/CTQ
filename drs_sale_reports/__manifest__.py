43e9dda77069e13f0a4f9a9e3b600b4758580bad
Alejandro <auralr@gmail.com>
2025-10-03 13:03:11 -0600

    [ADD] cambios en reportes

---
+++ drs_sale_reports/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Drasi | drs_sale_reports",
+    "summary": "Mejora en los formatos",
+    "description": """
+        Este módulo complementa los reportes de ventas, compras.
+    """,
+    "author": "Drasi Consulting",
+    "website": "https://www.drasi.odoo.com",
+    "category": "Sale",
+    "version": "17.0.1.0.0",
+    "depends": [
+        'sale',
+        'purchase',
+        'purchase_stock'
+    ],
+    "data": [
+        'views/report_saleorder_document.xml',
+        'views/purchase_quotation_templates.xml'
+    ],
+    "licence": "LGPL-3",
+}
