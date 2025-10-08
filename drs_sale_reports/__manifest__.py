fb3c62c852e4a5b950d4e9bb2fc84a3df7d11fc1
Alejandro <auralr@gmail.com>
2025-10-08 11:28:06 -0600

    [FIX] cambios en reportes de compra

--- drs_sale_reports/__manifest__.py
+++ drs_sale_reports/__manifest__.py
@@ -12,11 +12,13 @@
     "depends": [
         'sale',
         'purchase',
-        'purchase_stock'
+        'purchase_stock',
+        'l10n_mx_edi'
     ],
     "data": [
         'views/report_saleorder_document.xml',
-        'views/purchase_quotation_templates.xml'
+        'views/purchase_quotation_templates.xml',
+        'views/purchase_order_templates.xml',
     ],
     "licence": "LGPL-3",
 }
